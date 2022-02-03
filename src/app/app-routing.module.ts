9c570ee3e52a078d3ee5ba9cfe8284d491e055ce
Aayush Goel <42693590+aelit31134@users.noreply.github.com>
2022-02-03 10:38:28 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'home',
+  component: DefaultComponent,
+  children: [{
+    path: 'dashboard',
+    component: DashboardComponent
+  }, {
+    path: 'posts',
+    component: PostsComponent
+  }]
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
